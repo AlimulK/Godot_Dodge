7e592c261bcb0758181f9c0982993f889f3e8385
AlimulK <samskarim@gmail.com>
2023-03-31 11:50:51 +0100

    project complete

--- main.gd
+++ main.gd
@@ -32,6 +32,7 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_start_timer_timeout():
@@ -42,9 +43,13 @@ func _on_start_timer_timeout():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
